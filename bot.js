ec153343c1eaea39e3250e9262021de1b406564c
whatsupdawg <36465425+whatsupdawg@users.noreply.github.com>
2018-02-14 17:05:05 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,28 +34,20 @@ bot.on('message', msg =>{//object of type message, named "messsage"
     'Outlook not so good',
     'Very doubtful'
 ];//array size 20, index begins at 0
-	
-	  var jokes = [
-      'Two neutrons walk into a bar, and the bartender says, \"No charge for you.\"',
-      'What happens when a frog\'s car breaks down? It gets toad away!',
-      'Why did the hipster burn his tongue? He drank his coffee before it was cool.',
-      'Two bots walk into a Discord server...I forget the rest.'
-    ];
 
 
 	const args = msg.content.slice(prefix.length).trim().split(/ +/g);
  	const cmd = args.shift().toLowerCase();
 
 	if(cmd === 'joke'){
-		let i = Math.floor(Math.random()*jokes.length);
-		msg.channel.send(jokes[i]);
+		msg.channel.send('Two neutrons walk into a bar, and the bartender says \"No charge for you.\"');
 	}
 
 	else if(cmd === 'greet'){
 		msg.channel.send("Hi, "+msg.author.toString()+"!");
 	}
 
-	else if(cmd.startsWith('8ball')){
+	else if(cmd.startsWith("8ball")){
 		let i = answers[Math.floor(Math.random()*answers.length)];
 		const embed = {//this is an embed object
   "color": 0xffaeff,
@@ -83,7 +75,8 @@ msg.channel.send({ embed });
 
 		let choice = choices[rand];
 
-		msg.channel.send(`I choose:${choice}.`);
+		msg.channel.send("I choose:"+choice);
+	}
 
 
 	});
