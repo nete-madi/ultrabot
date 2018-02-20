f2137342ce265cb463e200830d1bd260bab2557c
whatsupdawg <36465425+whatsupdawg@users.noreply.github.com>
2018-02-20 11:41:06 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -92,7 +92,7 @@ var jokes = [
 			break;
 
 		case "getrole":
-			if(msg.author.roles.has("196796210164662273")) {
+			if(msg.member.roles.has("196796210164662273")) {
 				msg.channel.send(`You have this role!`);
 			} else {
 				msg.channel.send(`You do not have that role.`);
